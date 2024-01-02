a9a2083facce455d425dd9b2a27b91ef9aa05a44
Ali Asger <aliasgersaifee2003@gmail.com>
2024-01-02 10:18:04 +0530

    Created all cached articles entity classes.

--- app/src/main/java/com/justme/snapnews/data/db/cachedarticlesdb/CachedArticlesEntity.kt
+++ app/src/main/java/com/justme/snapnews/data/db/cachedarticlesdb/CachedArticlesEntity.kt
@@ -5,23 +5,226 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.justme.snapnews.data.models.NewsItem
 
-@Entity(tableName = "cached_articles")
-data class CachedArticlesEntity(
-    @PrimaryKey val timeStamp : String,
-    @ColumnInfo(name = "top") val top: MutableList<NewsItem>? = null,
-    @ColumnInfo(name = "technology") val technology: MutableList<NewsItem>? = null,
-    @ColumnInfo(name = "business") val business: MutableList<NewsItem>? = null,
-    @ColumnInfo(name = "crime") val crime: MutableList<NewsItem>? = null,
-    @ColumnInfo(name = "domestic") val domestic: MutableList<NewsItem>? = null,
-    @ColumnInfo(name = "education") val education: MutableList<NewsItem>? = null,
-    @ColumnInfo(name = "entertainment") val entertainment: MutableList<NewsItem>? = null,
-    @ColumnInfo(name = "environment") val environment: MutableList<NewsItem>? = null,
-    @ColumnInfo(name = "food") val food: MutableList<NewsItem>? = null,
-    @ColumnInfo(name = "health") val health: MutableList<NewsItem>? = null,
-    @ColumnInfo(name = "other") val other: MutableList<NewsItem>? = null,
-    @ColumnInfo(name = "politics") val politics: MutableList<NewsItem>? = null,
-    @ColumnInfo(name = "science") val science: MutableList<NewsItem>? = null,
-    @ColumnInfo(name = "sports") val sports: MutableList<NewsItem>? = null,
-    @ColumnInfo(name = "tourism") val tourism: MutableList<NewsItem>? = null,
-    @ColumnInfo(name = "world") val world: MutableList<NewsItem>? = null,
+@Entity(tableName = "top")
+data class TopCategory(
+    @PrimaryKey val article_id: String?,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "link") val link: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "category") val category: List<String>,
+    @ColumnInfo(name = "country") val country: List<String>,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "image_url") val image_url: String,
+    @ColumnInfo(name = "source_id") val source_id: String,
+    @ColumnInfo(name = "isBookmarked") val isBookmarked: Boolean
+)
+
+@Entity(tableName = "business")
+data class BusinessCategory(
+    @PrimaryKey val article_id: String?,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "link") val link: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "category") val category: List<String>,
+    @ColumnInfo(name = "country") val country: List<String>,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "image_url") val image_url: String,
+    @ColumnInfo(name = "source_id") val source_id: String,
+    @ColumnInfo(name = "isBookmarked") val isBookmarked: Boolean
+)
+
+@Entity(tableName = "technology")
+data class TechnologyCategory(
+    @PrimaryKey val article_id: String?,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "link") val link: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "category") val category: List<String>,
+    @ColumnInfo(name = "country") val country: List<String>,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "image_url") val image_url: String,
+    @ColumnInfo(name = "source_id") val source_id: String,
+    @ColumnInfo(name = "isBookmarked") val isBookmarked: Boolean
+)
+
+@Entity(tableName = "science")
+data class ScienceCategory(
+    @PrimaryKey val article_id: String?,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "link") val link: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "category") val category: List<String>,
+    @ColumnInfo(name = "country") val country: List<String>,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "image_url") val image_url: String,
+    @ColumnInfo(name = "source_id") val source_id: String,
+    @ColumnInfo(name = "isBookmarked") val isBookmarked: Boolean
+)
+
+@Entity(tableName = "crime")
+data class CrimeCategory(
+    @PrimaryKey val article_id: String?,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "link") val link: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "category") val category: List<String>,
+    @ColumnInfo(name = "country") val country: List<String>,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "image_url") val image_url: String,
+    @ColumnInfo(name = "source_id") val source_id: String,
+    @ColumnInfo(name = "isBookmarked") val isBookmarked: Boolean
+)
+
+@Entity(tableName = "domestic")
+data class DomesticCategory(
+    @PrimaryKey val article_id: String?,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "link") val link: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "category") val category: List<String>,
+    @ColumnInfo(name = "country") val country: List<String>,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "image_url") val image_url: String,
+    @ColumnInfo(name = "source_id") val source_id: String,
+    @ColumnInfo(name = "isBookmarked") val isBookmarked: Boolean
+)
+
+@Entity(tableName = "education")
+data class EducationCategory(
+    @PrimaryKey val article_id: String?,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "link") val link: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "category") val category: List<String>,
+    @ColumnInfo(name = "country") val country: List<String>,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "image_url") val image_url: String,
+    @ColumnInfo(name = "source_id") val source_id: String,
+    @ColumnInfo(name = "isBookmarked") val isBookmarked: Boolean
+)
+
+@Entity(tableName = "entertainment")
+data class EntertainmentCategory(
+    @PrimaryKey val article_id: String?,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "link") val link: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "category") val category: List<String>,
+    @ColumnInfo(name = "country") val country: List<String>,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "image_url") val image_url: String,
+    @ColumnInfo(name = "source_id") val source_id: String,
+    @ColumnInfo(name = "isBookmarked") val isBookmarked: Boolean
+)
+
+@Entity(tableName = "environment")
+data class EnvironmentCategory(
+    @PrimaryKey val article_id: String?,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "link") val link: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "category") val category: List<String>,
+    @ColumnInfo(name = "country") val country: List<String>,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "image_url") val image_url: String,
+    @ColumnInfo(name = "source_id") val source_id: String,
+    @ColumnInfo(name = "isBookmarked") val isBookmarked: Boolean
+)
+
+@Entity(tableName = "food")
+data class FoodCategory(
+    @PrimaryKey val article_id: String?,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "link") val link: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "category") val category: List<String>,
+    @ColumnInfo(name = "country") val country: List<String>,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "image_url") val image_url: String,
+    @ColumnInfo(name = "source_id") val source_id: String,
+    @ColumnInfo(name = "isBookmarked") val isBookmarked: Boolean
+)
+
+@Entity(tableName = "health")
+data class HealthCategory(
+    @PrimaryKey val article_id: String?,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "link") val link: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "category") val category: List<String>,
+    @ColumnInfo(name = "country") val country: List<String>,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "image_url") val image_url: String,
+    @ColumnInfo(name = "source_id") val source_id: String,
+    @ColumnInfo(name = "isBookmarked") val isBookmarked: Boolean
+)
+
+@Entity(tableName = "other")
+data class OtherCategory(
+    @PrimaryKey val article_id: String?,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "link") val link: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "category") val category: List<String>,
+    @ColumnInfo(name = "country") val country: List<String>,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "image_url") val image_url: String,
+    @ColumnInfo(name = "source_id") val source_id: String,
+    @ColumnInfo(name = "isBookmarked") val isBookmarked: Boolean
+)
+
+@Entity(tableName = "politics")
+data class PoliticsCategory(
+    @PrimaryKey val article_id: String?,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "link") val link: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "category") val category: List<String>,
+    @ColumnInfo(name = "country") val country: List<String>,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "image_url") val image_url: String,
+    @ColumnInfo(name = "source_id") val source_id: String,
+    @ColumnInfo(name = "isBookmarked") val isBookmarked: Boolean
+)
+
+@Entity(tableName = "sports")
+data class SportsCategory(
+    @PrimaryKey val article_id: String?,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "link") val link: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "category") val category: List<String>,
+    @ColumnInfo(name = "country") val country: List<String>,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "image_url") val image_url: String,
+    @ColumnInfo(name = "source_id") val source_id: String,
+    @ColumnInfo(name = "isBookmarked") val isBookmarked: Boolean
+)
+
+@Entity(tableName = "tourism")
+data class TourismCategory(
+    @PrimaryKey val article_id: String?,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "link") val link: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "category") val category: List<String>,
+    @ColumnInfo(name = "country") val country: List<String>,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "image_url") val image_url: String,
+    @ColumnInfo(name = "source_id") val source_id: String,
+    @ColumnInfo(name = "isBookmarked") val isBookmarked: Boolean
+)
+
+@Entity(tableName = "world")
+data class WorldCategory(
+    @PrimaryKey val article_id: String?,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "link") val link: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "category") val category: List<String>,
+    @ColumnInfo(name = "country") val country: List<String>,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "image_url") val image_url: String,
+    @ColumnInfo(name = "source_id") val source_id: String,
+    @ColumnInfo(name = "isBookmarked") val isBookmarked: Boolean
 )
